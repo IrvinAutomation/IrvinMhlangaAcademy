ba9f632b2edb2ff2b0ba7ac1c7e296666084638d
Irvin <sibusisoirvin@gmail.com>
2025-04-28 22:17:58 +0200

    Third commit

--- IrvinSeleniumTests/DropDownElements.cs
+++ IrvinSeleniumTests/DropDownElements.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace IrvinSeleniumTests
 {
@@ -11,7 +12,7 @@ namespace IrvinSeleniumTests
         [SetUp]
         public void StartBrowser()
         {
-            
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
         }
         public void SelectDropDown()
         {
