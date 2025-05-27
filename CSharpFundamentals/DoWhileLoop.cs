acf653c0eeeceb8817fad66b4cded2128fb029ec
Irvin <irvin.mhlanga@altron.com>
2025-05-27 22:02:18 +0200

    Do while and while loop

---
+++ CSharpFundamentals/DoWhileLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    public class DoWhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int index = 6;
+
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+
+            } while (index <= 1);
+        }
+    }
+}
---
+++ CSharpFundamentals/WhileLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    public class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            while (index <= 6)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+        }
+    }
+}
